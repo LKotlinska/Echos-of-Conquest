46e87d2ac5238ecade9af4dbadc4ab344e5d9d34
Laura Kotlinska <laura.kotlinska.96@gmail.com>
2026-04-13 10:11:30 +0200

    Add initial enemy list

--- src/EchoesOfConquest/Game.cs
+++ src/EchoesOfConquest/Game.cs
@@ -38,8 +38,29 @@ public class Game
         Console.WriteLine($"Welcome {name} the {playerClass.Name}");
         return new Player(name, playerClass);
     }
-    
-    
+
+    private List<Enemy> EnemyCreation()
+    {
+        _enemies = new List<Enemy>();
+
+        HealthPotion smallPotion = new HealthPotion("Small Potion", 15);
+        Enemy goblin = new Enemy("Goblin", 30, 2, 4, 10, smallPotion);
+
+        Weapon axe = new Weapon("Axe", 8);
+        Enemy orc = new Enemy("Orc", 50, 4, 6, 12, axe);
+
+        HealthPotion largePotion = new HealthPotion("Large Potion", 30);
+        Enemy skeleton = new Enemy("Skeleton", 40, 3, 6, 13, largePotion);
+
+        Weapon magicStaff = new Weapon("Magic Staff", 6, "Wizard");
+        Enemy darkMage = new Enemy("Dark Mage", 60, 5, 8, 11, magicStaff);
+
+        Weapon rapiers = new Weapon("Rapiers", 7, "Rogue");
+        Enemy dragon = new Enemy("Dragon", 100, 7, 10, 15, rapiers);
+        
+        _enemies.AddRange(new List<Enemy>() {goblin, orc, skeleton, darkMage, dragon});
+        return _enemies;
+    }
     
     
     
